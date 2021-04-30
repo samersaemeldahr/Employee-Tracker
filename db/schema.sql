ee7f5977a652c67ddc6e27443b921725f22a5a16
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-01 00:14:39 +0300

    Create role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  INDEX department_index (department_id),
+  CONSTRAINT foreignkey_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
