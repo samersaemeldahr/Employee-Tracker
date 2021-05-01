528bd8d24fb930181acb28a0808fb813d5bd2980
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-01 04:19:20 +0300

    Create employee table

--- db/schema.sql
+++ db/schema.sql
@@ -18,3 +18,16 @@ CREATE TABLE role (
   CONSTRAINT foreignkey_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX manager_index (manager_id),
+  CONSTRAINT foreignkey_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+  PRIMARY KEY (id)
+);
