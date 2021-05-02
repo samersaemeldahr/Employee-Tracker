e86238224222a88e19a440c29e9657156284a9bf
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-03 02:27:55 +0300

    Insert values into employee table

--- db/seed.sql
+++ db/seed.sql
@@ -13,3 +13,12 @@ VALUES  ('Sales Lead', 150000, 1),
         ('Legal Team Lead', 200000, 4),
         ('Lawyer', 100000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Jimi', 'Hendrix', 1, NULL),
+        ('Eric', 'Clapton', 2, 1),
+        ('John Lee', 'Hooker', 3, NULL),
+        ('Stevie Ray', 'Vaughan', 4, 3),
+        ('Sonny Boy', 'Williamson', 5, NULL),
+        ('Albert', 'King', 6, 5),
+        ('Willie', 'Dixon', 7, NULL),
+        ('Bonnie', 'Raitt', 8, 7);
