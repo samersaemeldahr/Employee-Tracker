9d15ebb2fe7ab93b8022ebac2372e4c095577500
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-03 02:27:15 +0300

    Insert values into role table

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,13 @@ use employees;
 INSERT INTO department (name)
 VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Sales Lead', 150000, 1),
+        ('Salesperson', 90000, 1),
+        ('Lead Engineer', 200000, 2),
+        ('Software Engineer', 150000, 2),
+        ('Account Manager', 120000, 3),
+        ('Accountant', 100000, 3),
+        ('Legal Team Lead', 200000, 4),
+        ('Lawyer', 100000, 4);
 
