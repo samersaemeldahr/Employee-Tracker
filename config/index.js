f894139f8a226b7ed79834479578dfcddd793a28
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-04 03:58:01 +0300

    Add find employees by manager and department queries

--- config/index.js
+++ config/index.js
@@ -16,6 +16,18 @@ class Database {
     );
   }
 
+  allEmployeesByDepartment(departmentId) {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;", departmentId
+    );
+  }
+
+  allEmployeesByManager(managerId) {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;", managerId
+    );
+  }
+
   removeEmployee(employeeId) {
     return this.connection.query("DELETE FROM employee WHERE id = ?", employeeId);
   }
