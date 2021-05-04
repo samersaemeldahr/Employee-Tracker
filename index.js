b63c901708e2c1db88e6c4e9bb3946d15567a8c9
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-05 00:30:12 +0300

    Create main prompts

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-const inquirer = require("inquirer");
-const db = require("./db");
-const config = require("./config");
+const config = require("./db");
+const {prompt} = require("inquirer");
 require("console.table");
+
+// Ask main questions
+async function mainQuestions() {
+    const choice = await prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "ALL_EMPLOYEES"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "ALL_EMPLOYEES_BY_DEPARTMENT"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                },
+                {
+                    name: "View All Roles",
+                    value: "ALL_ROLES"
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE"
+                },
+                {
+                    name: "View All Departments",
+                    value: "ALL_DEPARTMENTS"
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT"
+                }
+            ]
+        }
+    ]);
+}
+
+mainQuestions();
