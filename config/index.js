9656a2d96924a0796569b3148e6020c688559454
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-04 04:01:00 +0300

    Add department queries

--- config/index.js
+++ config/index.js
@@ -31,6 +31,20 @@ class Database {
   removeEmployee(employeeId) {
     return this.connection.query("DELETE FROM employee WHERE id = ?", employeeId);
   }
+
+  createDepartment(department) {
+    return this.connection.query("INSERT INTO department SET ?", department);
+  }
+
+  allDepartments() {
+    return this.connection.query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
+    );
+  }
+
+  removeDepartment(departmentId) {
+    return this.connection.query("DELETE FROM department WHERE id = ?", departmentId);
+  }
 }
 
 module.exports = new Database(connection);
