41632321e78c8e37fd13691b09548c0ed9996458
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-04 03:05:24 +0300

    Add create Employee query

--- config/index.js
+++ config/index.js
@@ -1,8 +1,13 @@
 const connection = require("./connection");
 
 class Database {
+
   constructor(connection) {
     this.connection = connection;
+  }
+
+  createEmployee(employee) {
+    return this.connection.query("INSERT INTO employee SET ?", employee);
   }
 }
 
