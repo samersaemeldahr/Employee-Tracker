2d5e96915b50a123dfe7e8035056cf532a529beb
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-05 04:24:47 +0300

    Create add employee, role and department functions

--- index.js
+++ index.js
@@ -70,17 +70,17 @@ async function mainQuestions() {
             return allEmployeesByDepartment();
         case "ADD_EMPLOYEE":
             return addEmployee();
-        case "REMOVE_EMPLOYEE":
-            return removeEmployee();
         case "ADD_DEPARTMENT":
             return addDepartment();
-        case "REMOVE_DEPARTMENT":
-            return removeDepartment();
         case "ADD_ROLE":
             return addRole();
+        case "REMOVE_EMPLOYEE":
+            return removeEmployee();
+        case "REMOVE_DEPARTMENT":
+            return removeDepartment();
         case "REMOVE_ROLE":
             return removeRole();
-        default:
+        case "Quit":
             return quit();
     }
 }
@@ -115,6 +115,109 @@ async function allDepartments() {
     const departments = await config.allDepartments();
 
     console.table(departments);
+
+    mainQuestions();
+}
+
+// Create add employee function
+async function addEmployee() {
+    const roles = await config.allRoles();
+    const employees = await config.allEmployees();
+
+    const employee = await prompt([
+        {
+            name: "first_name",
+            message: "What is the employee's first name?"
+        },
+        {
+            name: "last_name",
+            message: "What is the employee's last name?"
+        }
+    ]);
+
+    const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+
+    const { roleId } = await prompt({
+        type: "list",
+        name: "roleId",
+        message: "What is the employee's role?",
+        choices: roleChoices
+    });
+
+    employee.role_id = roleId;
+
+    const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+    managerChoices.unshift({ name: "None", value: null });
+
+    const { managerId } = await prompt({
+        type: "list",
+        name: "managerId",
+        message: "Who is the employee's manager?",
+        choices: managerChoices
+    });
+
+    employee.manager_id = managerId;
+
+    await config.createEmployee(employee);
+
+    console.log(
+        `Added ${employee.first_name} ${employee.last_name} to the database`
+    );
+
+    mainQuestions();
+}
+
+// Create add department function
+async function addDepartment() {
+    const department = await prompt([
+        {
+            name: "name",
+            message: "What is the name of the department?"
+        }
+    ]);
+
+    await config.createDepartment(department);
+
+    console.log(`Added ${department.name} to the database`);
+
+    mainQuestions();
+}
+
+// Create add role function
+async function addRole() {
+    const departments = await config.allDepartments();
+
+    const departmentChoices = departments.map(({ id, name }) => ({
+        name: name,
+        value: id
+    }));
+
+    const role = await prompt([
+        {
+            name: "title",
+            message: "What is the name of the role?"
+        },
+        {
+            name: "salary",
+            message: "What is the salary of the role?"
+        },
+        {
+            type: "list",
+            name: "department_id",
+            message: "Which department does the role belong to?",
+            choices: departmentChoices
+        }
+    ]);
+
+    await config.createRole(role);
+
+    console.log(`Added ${role.title} to the database`);
 
     mainQuestions();
 }
