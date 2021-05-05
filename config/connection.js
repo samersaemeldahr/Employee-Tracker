30f7f514f0636577e9ebf647996badd9f5215c1e
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-05 05:16:20 +0300

    Fix directory

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql");
+const util = require("util");
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "employees"
+  database: "team_db"
 });
 
 connection.connect();
+
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
